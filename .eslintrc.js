c194330880205ab7f89b6fe58e7069c239fc751b
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-09-20 15:25:21 -0300

    Removing the bypass on no-unused-vars in eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2020': true,
+  env: {
+    browser: true,
+    es2020: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'google',
-  ],
-  'overrides': [
+  extends: ["eslint:recommended", "google"],
+  overrides: [
     {
-      'files': ['**/*.svelte'],
-      'processor': 'svelte3/svelte3',
+      files: ["**/*.svelte"],
+      processor: "svelte3/svelte3",
     },
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  'plugins': [
-    '@typescript-eslint',
-    'svelte3',
-    'prettier',
-  ],
-  'rules': {
-    'max-len': ['error', { code: 100 }],
-    'require-jsdoc': ['off'],
-    'no-unused-vars': ['off'], // it is going crazy with imported types
+  plugins: ["@typescript-eslint", "svelte3", "prettier"],
+  rules: {
+    "max-len": ["error", { code: 100 }],
+    "require-jsdoc": ["off"],
   },
 };
